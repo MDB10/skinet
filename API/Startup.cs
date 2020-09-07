2c78e5c8b34fb530c2d70dc919b3fe627ea955d5
Oumar Sidibe <barridara@yahoo.fr>
2020-09-07 21:04:08 +0200

    Fin de la section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-using API.Data;
+using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
+
 
 namespace API
 {
@@ -21,6 +21,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
